450187aafd1cf96ce7a1eb52c1c27a0134607367
Paul <mail@paulcbauer.eu>
2015-11-07 13:51:07 +0100

    Added description/license and links.

--- ui.R
+++ ui.R
@@ -26,7 +26,17 @@ library(plotrix)
       sliderInput("size_systematic_error", label = "Add systematic error:",
                   min = -20, max = 20, value = 0, step = 1),
       sliderInput("size_random_error", label = "Add random error:",
-                  min = 0, max = 20, value = 0, step = 1)
+                  min = 0, max = 20, value = 0, step = 1),
+      br(), br(),
+      
+      div("Shiny app by", 
+          a(href="http://paulcbauer.eu/",target="_blank", 
+            "Paul C. Bauer"),align="right", style = "font-size: 8pt"),
+      
+      
+      div("Shiny/R code:",
+          a(href="https://github.com/paulbauer/measurement_error/",
+            target="_blank","GitHub"),align="right", style = "font-size: 8pt")
 
 
 
